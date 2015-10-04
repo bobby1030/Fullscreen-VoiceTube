ade7b8ae286e4541323e7be03964ddcc2c500054
BobbyHo <angela.bobby@gmail.com>
2015-10-04 15:31:26 +0800

    #Update

--- fullscreenVoiceTube.user.js
+++ fullscreenVoiceTube.user.js
@@ -6,8 +6,8 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @version     1
 // @grant       none
-// @updateURL https://cdn.rawgit.com/bobby1030/Fullscreen-VoiceTube/master/fullscreenVoiceTube.user.js
-// @icon http://tw.blog.voicetube.com/wp-content/uploads/2014/07/unnamed-2.png
+// @updateURL   https://cdn.rawgit.com/bobby1030/Fullscreen-VoiceTube/master/fullscreenVoiceTube.user.js
+// @icon        http://tw.blog.voicetube.com/wp-content/uploads/2014/07/unnamed-2.png
 // ==/UserScript==
 
 jQuery.noConflict()
